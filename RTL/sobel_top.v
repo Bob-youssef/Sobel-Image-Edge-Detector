a9b64c8fa6f7c16e41abdd00a77fd41ee05aba71
Marc Victor <marc.victor2005@gmail.com>
2026-08-26 16:48:58 +0300

    Include line_buffer and sobel_math instantiations in sobel_top.v

--- RTL/sobel_top.v
+++ RTL/sobel_top.v
@@ -16,4 +16,30 @@ module sobel_top (
     wire [7:0] p31, p32, p33;
     wire        window_valid;
 
+    line_buffer u_line_buffer (
+        .clk           (clk),
+        .reset         (reset),
+        .pixel_in      (pixel_in),
+        .data_valid_in (data_valid_in),
+
+        .p11 (p11), .p12 (p12), .p13 (p13),
+        .p21 (p21), .p22 (p22), .p23 (p23),
+        .p31 (p31), .p32 (p32), .p33 (p33),
+
+        .window_valid (window_valid)
+    );
+
+    sobel_math u_sobel_math (
+        .clk   (clk),
+        .reset (reset),
+
+        .p11 (p11), .p12 (p12), .p13 (p13),
+        .p21 (p21), .p22 (p22), .p23 (p23),
+        .p31 (p31), .p32 (p32), .p33 (p33),
+        .window_valid (window_valid),
+
+        .pixel_out      (pixel_out),
+        .data_valid_out (data_valid_out)
+    );
+
 endmodule
